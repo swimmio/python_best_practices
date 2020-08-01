f6d6d32bf67e878a3644f44be88de8ce82fe331c
omerr <omer@swimm.io>
2020-08-01 12:25:24 +0300

    Split to files

--- common_libs.py
+++ common_libs.py
@@ -1,4 +1,3 @@
-import collections
 import math
 from multiprocessing import Pool
 
@@ -43,19 +42,3 @@ def counting_occurrences(iterable, element):
     return collections.Counter(iterable)[element]
 
 
-Point = collections.namedtuple('Point', ['x', 'y'])
-
-
-def add_points(p1, p2):
-    """
-    >>> p1 = Point(x=0, y=0)
-    >>> p2 = Point(1, 2)
-    >>> p3 = add_points(p1, p2)
-    >>> p3
-    Point(x=1, y=2)
-    >>> p3[0] == p3.x
-    True
-    >>> p3 == p2
-    True
-    """
-    return Point(p1.x + p2.x, p1.y + p2.y)
