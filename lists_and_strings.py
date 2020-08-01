b501a34faa45d542f55b8a7d83a5607db3bd9346
omerr <omer@swimm.io>
2020-08-02 00:02:49 +0300

    split `print_with_indexes` to a separate file

--- lists_and_strings.py
+++ lists_and_strings.py
@@ -1,17 +1,3 @@
-def print_with_indexes(my_list):
-    """
-    Prints on every line: <index>: <value>
-    For example:
-    >>> print_with_indexes(['a', 'b', 'c'])
-    0: a
-    1: b
-    2: c
-    """
-    # Using `enumerate` is very elegant in this case
-    for (i, value) in enumerate(my_list):
-        print('{}: {}'.format(i, value))
-
-
 def last_element(my_list_or_string):
     """
     Returns the last element of a list or string
