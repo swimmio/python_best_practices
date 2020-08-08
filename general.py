b0fc7992c0c28271762b1cc0049b930de81d4dca
omerr <omer@swimm.io>
2020-08-08 23:50:03 +0300

    split print_with_underscores to a standalone file

--- general.py
+++ general.py
@@ -1,12 +1,3 @@
-def print_with_underscores(my_list):
-    """
-    Using only `print`, this function prints all elements of `my_list`, with two underscores between elements.
-    >>> print_with_underscores(["hello", "world", "from", "swimm"])
-    hello__world__from__swimm
-    """
-    print(*my_list, sep='__')
-
-
 def convert_to_decimal(input_number, base):
     """
     Returns the decimal representation of `input_number` given in base `base`.
