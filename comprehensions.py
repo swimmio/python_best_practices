e546a56b29a705fc985c55a63a25d350d24a5a99
omerr <omer@swimm.io>
2020-08-09 10:57:53 +0300

    Split squared_list to a separate file

--- comprehensions.py
+++ comprehensions.py
@@ -1,13 +1,3 @@
-def squared_list(iterable):
-    """
-    Returns a list where every element of `iterable` is squared.
-    Assumes all elements are ints.
-    >>> squared_list([1,2,3])
-    [1, 4, 9]
-    """
-    return [x ** 2 for x in iterable]
-
-
 def squared_for_even_numbers(iterable):
     """
     Returns a list where every even element of `iterable` is squared.
