84321d1e34915243d1fe691d9b9193d2e7bacc29
omerr <omer@swimm.io>
2020-08-09 18:28:16 +0300

    Split sum of keys and merge dicts

--- sum_of_keys.py
+++ sum_of_keys.py
@@ -18,13 +18,3 @@ def sum_of_keys(input_dict):
         return_dict[value] += key
 
     return return_dict
-
-
-def merge_dicts(dict_1, dict_2, dict_3):
-    """
-    Returns a dict that is a merge of the three input dicts.
-    In case of duplicate keys, the last dictionary to have the key "wins".
-    >>> merge_dicts({'a': 1, 'b': 1}, {'b': 2, 'c': 2}, {'e': 3, 'b': 3})
-    {'a': 1, 'b': 3, 'c': 2, 'e': 3}
-    """
-    return {**dict_1, **dict_2, **dict_3}
