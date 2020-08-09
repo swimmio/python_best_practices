e695f948fcc790e18a617d7dc9e815d7eb08a130
omerr <omer@swimm.io>
2020-08-09 11:02:13 +0300

    Split squared_generator to a separate file

--- comprehensions.py
+++ comprehensions.py
@@ -1,16 +1,3 @@
-def squared_generator(iterable):
-    """
-    Returns a generator that yields elements of `iterable`, with their value - squared.
-    Assumes all elements are ints.
-    >>> g = squared_generator([1,2,3])
-    >>> type(g)
-    <class 'generator'>
-    >>> list(g)
-    [1, 4, 9]
-    """
-    return (x ** 2 for x in iterable)
-
-
 def squared_set(iterable):
     """
     Returns a list where every element of `iterable` is squared.
