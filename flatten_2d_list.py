869d635a802ce280ad0a7ecbaa539971b63fbf65
omerr <omer@swimm.io>
2020-08-27 18:03:49 +0300

    Corrected description

--- flatten_2d_list.py
+++ flatten_2d_list.py
@@ -1,6 +1,6 @@
 def flatten_2d_list(my_list):
     """
-    Takes a list of two-members elements, and prints two flat lists.
+    Takes a list of two-members elements, and prints two flat tuples.
     >>> flatten_2d_list([['a', 'b'], ['c', 'd'], ['e', 'f']])
     ('a', 'c', 'e')
     ('b', 'd', 'f')
