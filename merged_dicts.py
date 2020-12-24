6009c46b567e6d99702d9805c7baab128f33cedd
omerr <omer@swimm.io>
2020-12-24 09:10:22 +0200

    Fix test for merged_dicts

--- merged_dicts.py
+++ merged_dicts.py
@@ -2,7 +2,10 @@ def merge_dicts(dict_1, dict_2, dict_3):
     """
     Returns a dict that is a merge of the three input dicts.
     In case of duplicate keys, the last dictionary to have the key "wins".
-    >>> merge_dicts({'a': 1, 'b': 1}, {'b': 2, 'c': 2}, {'e': 3, 'b': 3})
-    {'a': 1, 'b': 3, 'c': 2, 'e': 3}
+    
+    # Running the following line sould result in: {'a': 1, 'b': 3, 'c': 2, 'e': 3}
+    >>> merged_dict = merge_dicts({'a': 1, 'b': 1}, {'b': 2, 'c': 2}, {'e': 3, 'b': 3})
+    >>> sorted(merged_dict.items())
+    [('a', 1), ('b', 3), ('c', 2), ('e', 3)]
     """
     return {**dict_1, **dict_2, **dict_3}
