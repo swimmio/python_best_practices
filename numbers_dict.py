46810fee9316e83414c3c8dc1b71425b19b48181
omerr <omer@swimm.io>
2021-01-22 18:08:30 +0200

    modify numbers_dict to shift to a walkthrough

--- numbers_dict.py
+++ numbers_dict.py
@@ -1,6 +1,13 @@
 def numbers_dict():
     """
-    >>> numbers_dict()
-    {'one': 1, 'thousand': 1000, 'million': 1000000}
+    >>> numbers = numbers_dict()
+    >>> numbers['one']
+    1
+    >>> numbers['thousand']
+    1000
+    >>> numbers['million']
+    1000000
+    >>> 1_000 == 1_0_0_0 == 1000 == 100_0
+    True
     """
     return {'one': 1, 'thousand': 1_000, 'million': 1_000_000}
